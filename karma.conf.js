91c2a8473169d26fb9959d0f9da004a24eba0e74
Dorababu <dorababu.meka@otsi-usa.com>
2023-06-04 21:51:54 +0530

    removed name

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/otsi.portal.web'),
+      dir: require('path').join(__dirname, './coverage/portal.web'),
       subdir: '.',
       reporters: [
         { type: 'html' },
